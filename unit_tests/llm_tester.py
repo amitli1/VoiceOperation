2063e687f665184d4f6407d29be67624dde91e6b
amitli <amitli@rafael.co.il>
2025-12-03 17:37:40 +0200

    add examples

--- unit_tests/llm_tester.py
+++ unit_tests/llm_tester.py
@@ -4,16 +4,16 @@ import logging
 llm_tokenizer, llm_model, system_prompt = load_llm()
 
 def run_test(user_text, gt_command):
-    command = run_llm(llm_tokenizer, llm_model, system_prompt, text)
-    if command != gt_command:
+    user_command = run_llm(llm_tokenizer, llm_model, system_prompt, user_text)
+    user_command = user_command['command']
+    if user_command != gt_command:
         logging.info(f'Failed: {user_text}')
     else:
         logging.info(f'PASS: {user_text}')
 
 if __name__ == "__main__":
 
-
-    #text = "open general"
-    text = "how much battery do I have?"
-    command = run_llm(llm_tokenizer, llm_model, system_prompt, text)
-    logging.info(f'Command: {command}')
+    run_test(user_text="open general", gt_command="http://localhost:8080/show_overview")
+    run_test(user_text="how much battery do I have?", gt_command="http://localhost:8080/show_power_screen")
+    run_test(user_text="show me the map", gt_command="http://localhost:8080/show_navigation")
+    run_test(user_text="how many waepons do we have ?", gt_command="http://localhost:8080/show_inventory")
